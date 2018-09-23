c17b484f3e76ee36309e7884428a22a1644bc162
Npikielny <42814781+Npikielny@users.noreply.github.com>
2018-09-23 09:25:29 -0400

    Updated from Brython Server: 9/23/2018 9:25:29 AM

--- finalproject.py
+++ finalproject.py
@@ -13,15 +13,22 @@ noLine  = LineStyle(0, black)
 #-----------------------------------------------------
 class point(Sprite):
     def __init__(self, position, func):
-        funcColor = Color(0x000000, 1.0)
+        funcOut = int(str(func)*6)
+        funcColor = Color(0x0 + funcOut, 1.0)
         pt = CircleAsset(5, noLine, funcColor)
         Sprite(pt, position)
+    def color(red, green, blue):
+        letters = {10:"A",11:"B",12:"C",13:"D",14:"E",15:"F"}
+        output = ""
+        for i in (red, green, blue):
+            if floor(i / 16) >= 10:
 class Grapher(App):
     def __init__(self, width, height):
         super().__init__(width, height)
-        Sprite(RectangleAsset(width, height, noLine, black))
-        for i in range(0,11):
+        Sprite(RectangleAsset(width, height, noLine, white))
+        for i in range(0,10):
             point((i*5, 0), i)
+        point.color(1,2,3)
 
         
 
