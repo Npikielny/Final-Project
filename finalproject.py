cfd242569f0be86b7bc3ae051ca0ea5cff541a0b
Npikielny <42814781+Npikielny@users.noreply.github.com>
2018-09-25 12:10:06 -0400

    Updated from Brython Server: 9/25/2018 12:10:00 PM

--- finalproject.py
+++ finalproject.py
@@ -16,7 +16,7 @@ class point(Sprite):
     def __init__(self, position, color):
         pt = CircleAsset(5, noLine, color)
         Sprite(pt, position)
-    def color(red, green, blue):
+    def color(red, green, blue, alpha):
         letters = {10:"A",11:"B",12:"C",13:"D",14:"E",15:"F"}
         output = "0x"
         for i in (int(red), int(green), int(blue)):
@@ -29,13 +29,21 @@ class point(Sprite):
                 output += str(letters[i - a*16])
             else:
                 output += str(i - a*16)
-        return (Color(output, 1.0))
+        return (Color(output, alpha))
     def colorRandom(funcIndex):
-        return point.color(abs(sin(funcIndex*0.2)*255),abs(cos(funcIndex*1.31)*255),abs(cos(2*funcIndex)*sin(funcIndex*0.5)*255))
+        return point.color(abs(sin(funcIndex*0.2)*255),abs(cos(funcIndex*1.31)*255),abs(cos(2*funcIndex)*sin(funcIndex*0.5)*255), 1.0)
 class collectionViewCell(Sprite):
-    cell = RectangleAsset(200,50, LineStyle(1, point.color(100,100,100)), point.color(200,200,200))
+    cell = RectangleAsset(200,50, LineStyle(1, point.color(100,100,100,0.5)), point.color(200,200,200,0.5))
     def __init__(self, position):
         super().__init__(collectionViewCell.cell, position)
+class collectionView(Sprite):
+    cells = []
+    view = RectangleAsset(240,frameHeight, noLine, black)
+    def __init__(self, position):
+        super().__init__(collectionView.view, position)
+        for i in cells:
+            print("yay")
+        #    collectionViewCell((20, 10 + i * (50 + 10)))
 class function():
     functions = {}
     def __init__(self, inputString):
@@ -45,10 +53,13 @@ class function():
 class Grapher(App):
     def __init__(self, width, height):
         super().__init__(width, height)
-        Sprite(RectangleAsset(width, height, noLine, white), (0,0))
+        #Sprite(RectangleAsset(width, height, noLine, white), (0,0))
         for i in range(0,40):
             point((i*10,0), point.colorRandom(i))
-        collectionViewCell((10,10))
+        #collectionViewCell((20,10))
+        for i in range(0,4):
+            collectionView.cells.append(i)
+        collectionView((0,0))
     def function(inputString):
         functionType = 0
         left = 0
