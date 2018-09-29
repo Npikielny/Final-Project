93b943d76bc975c6acc0b73a1432f5d2896bc398
Npikielny <42814781+Npikielny@users.noreply.github.com>
2018-09-29 12:23:11 -0400

    Updated from Brython Server: 9/29/2018 12:23:11 PM

--- Test.py
+++ Test.py
@@ -1,1 +1,25 @@
+from ggame import App, Color, LineStyle, Sprite, CircleAsset, Frame, RectangleAsset
+red = Color(0xff0000, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+green  = Color(0x0fff6f, 1.0)
+white = Color(0xffffff, 1.0)
+#-----------------------------------------------------
+frameWidth = 800
+frameHeight = 800
+#-----------------------------------------------------
+#-----------------------------------------------------
+noLine  = LineStyle(0, black)
 
+class spriteType(Sprite):
+    def __init__(self,x,y):
+        print(x,y)
+
+class Game(App):
+    a = 4
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        super().spriteType(a,a)
+
+myapp = Game(frameWidth, frameHeight)
+myapp.run()
