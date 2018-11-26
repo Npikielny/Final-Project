8d86ef5ed00b5bec28560d15b71467dd28b4035b
Npikielny <42814781+Npikielny@users.noreply.github.com>
2018-11-26 17:18:20 -0500

    Updated from Brython Server: 11/26/2018 5:18:20 PM

--- Color.py
+++ Color.py
@@ -20,6 +20,7 @@ def color(red, green, blue, alpha):
 
 print(Color(0x050505, 1.0))
 print(color(5,5,5,1.0))
+print(Color(int("0x050505"),1.0))
 #print(type(0x000000))
 #print(Color(int("0x050505"),0.1))
 #print(int("0x050505"))
